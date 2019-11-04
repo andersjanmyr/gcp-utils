51a4fd719db6c74ef80be4601e5cbe380664c852
Anders Janmyr <anders@janmyr.com>
2019-11-04 17:06:05 +0100

    Removed string interpolation

--- website-bucket-cdn/main.tf
+++ website-bucket-cdn/main.tf
@@ -39,14 +39,14 @@ resource "google_storage_bucket" "bucket" {
 
 resource "google_storage_bucket_object" "index" {
   provider = "google-beta"
-  bucket = "${google_storage_bucket.bucket.name}"
+  bucket = google_storage_bucket.bucket.name
   name   = "index.html"
   source = "./index.html"
 }
 
 resource "google_storage_bucket_iam_binding" "binding" {
   provider = "google-beta"
-  bucket = "${google_storage_bucket.bucket.name}"
+  bucket = google_storage_bucket.bucket.name
   role = "roles/storage.objectViewer"
 
   members = [
@@ -57,7 +57,7 @@ resource "google_storage_bucket_iam_binding" "binding" {
 resource "google_compute_backend_bucket" "backend_bucket" {
   provider = "google-beta"
   name = "${var.name}-bb"
-  bucket_name = "${google_storage_bucket.bucket.name}"
+  bucket_name = google_storage_bucket.bucket.name
   enable_cdn = true
 }
 
@@ -72,7 +72,7 @@ resource "google_dns_record_set" "dns_record" {
   type = "A"
   ttl = 300
   managed_zone= var.dns_zone
-  rrdatas = ["${google_compute_global_address.ga.address}"]
+  rrdatas = [google_compute_global_address.ga.address]
 }
 
 resource "google_compute_managed_ssl_certificate" "cert" {
@@ -86,21 +86,21 @@ resource "google_compute_managed_ssl_certificate" "cert" {
 resource "google_compute_url_map" "url_map" {
   provider = "google-beta"
   name = "${var.name}-map"
-  default_service = "${google_compute_backend_bucket.backend_bucket.self_link}"
+  default_service = google_compute_backend_bucket.backend_bucket.self_link
 }
 
 resource "google_compute_target_https_proxy" "proxy" {
   provider = "google-beta"
   name="${var.name}-proxy"
-  url_map = "${google_compute_url_map.url_map.self_link}"
-  ssl_certificates = ["${google_compute_managed_ssl_certificate.cert.self_link}"]
+  url_map = google_compute_url_map.url_map.self_link
+  ssl_certificates = [google_compute_managed_ssl_certificate.cert.self_link]
 }
 
 resource "google_compute_global_forwarding_rule" "rule" {
   provider = "google-beta"
   name = "${var.name}-forwarding-rule"
-  ip_address = "${google_compute_global_address.ga.address}"
-  target = "${google_compute_target_https_proxy.proxy.self_link}"
+  ip_address = google_compute_global_address.ga.self_link
+  target = google_compute_target_https_proxy.proxy.self_link
   port_range = "443"
 
 }
